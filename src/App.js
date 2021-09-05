8d64e2660262a797be22f5aa5a222a7c16184cdd
hlabay22 <62175133+hlabay22@users.noreply.github.com>
2021-09-06 02:17:22 +0300

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 import './App.css';
 
 function App() {
   return (
-    <h2>Portfolio Project</h2>
+    <Navbar />
    
   );
 }
