e6d871410d8194caaec4929a713f46d0f8dd3806
hlabay22 <62175133+hlabay22@users.noreply.github.com>
2021-09-06 23:27:06 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import './App.css';
 
 function App() {
   return (
+    <>
+    <Particles 
+      params={{
+        Particles:{
+          number: {
+            value:30, 
+            density: {
+              enable : true,
+              value_area : 900
+            }
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 6,
+              color: "#f9ab00"
+            }
+          }
+        }
+      }}
+    />
     <Navbar />
-   
+    <Header />
+   </>
   );
 }
 
